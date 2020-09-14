26b37a22a2547d44cdd60df41aab127c10772f0b
Syaiful Ramadhan <syaifulramadhan714@gmail.com>
2020-09-14 09:21:12 +0700

    Coba Koding Kalkulator Sederhana

--- coba.py
+++ coba.py
@@ -1,1 +1,47 @@
-print("asw")
+'''
+Program kalkulator sederhana untuk menjumlah, mengurang, mengali, dan membagi bilangan dengan menggunakan fungsi
+'''
+
+# fungsi penjumlahan
+def add( x , y ) :
+    return x + y
+
+# fungsi pengurangan
+def subtract( x , y ) :
+    return x - y
+
+# fungsi perkalian
+def multiply( x , y ) :
+    return x * y
+
+# fungsi pembagian
+def divide( x , y ) :
+    return x / y
+
+# menu operasi
+print("Pilih Operasi.")
+print("1.Jumlah")
+print("2.Kurang")
+print("3.Kali")
+print("4.Bagi")
+
+# meminta input dari user
+choice = input("Masukan pilihan(1/2/3/4): ")
+
+num1 = int(input("Masukan bilangan pertama: "))
+num2 = int(input("Masukan bilangan kedua: "))
+
+if choice == '1' :
+    print( num1 , "+" , num2 , "=" , add( num1 , num2 ))
+
+elif choice == '2' :
+    print( num1 , "-" , num2 , "=" , subtract( num1 , num2 ))
+
+elif choice == '3' :
+    print( num1 , "*" , num2 , "=" , multiply( num1 , num2 ))
+
+elif choice == '4' :
+    print( num1 , "/" , num2 , "=" , divide( num1 , num2 ))
+
+else :
+    print("Inputan Salah")
